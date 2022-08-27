5af0b178356c5f70111d4a79a4eeecf76c0235cf
EricShyu <ericsshyus@gmail.com>
2022-08-26 20:58:41 -0400

    game complete, reset menu bug

--- src/scripts/script1.js
+++ src/scripts/script1.js
@@ -1,7 +1,7 @@
 //objects
 
 let GameBoard = (() => {
-  const _gameBoard = new Array(9);
+  let _gameBoard = new Array(9);
   let gameBoard = document.querySelector(".game-board-container");
 
   let generateGameBoard = () => {
@@ -23,7 +23,15 @@ let GameBoard = (() => {
       gameBoard.appendChild(tile);
     }
   };
-
+  let setGameBoard = (id, input) => {
+    _gameBoard[id] = input;
+  };
+  let getGameBoard = () => {
+    return _gameBoard;
+  };
+  let resetGameBoard = () => {
+    _gameBoard = new Array(9);
+  };
   let updateDom = () => {
     removeDom();
     generateGameBoard();
@@ -35,7 +43,15 @@ let GameBoard = (() => {
     }
   };
 
-  return { generateGameBoard, _gameBoard, updateDom };
+  return {
+    generateGameBoard,
+    _gameBoard,
+    updateDom,
+    removeDom,
+    getGameBoard,
+    resetGameBoard,
+    setGameBoard,
+  };
 })();
 
 let Player = (marker) => {
@@ -54,16 +70,28 @@ let GameLogic = (() => {
 
   //sets tile to x or o upon click
   let claimTile = (id) => {
-    GameBoard._gameBoard[id] = { class: _currentPlayer };
+    console.log("sssssssssssssss");
+    //set gameboard
+    GameBoard.setGameBoard(id, { class: _currentPlayer });
+    //GameBoard._gameBoard[id] = { class: _currentPlayer };
+    console.log(GameBoard._gameBoard[id]);
     setCurrentPlayer();
     console.log(_currentPlayer);
+    console.log(_turnCount);
     _turnCount++;
+
     GameBoard.updateDom();
+    //game over checks
+    checkHorizontal();
+    checkVertical();
+    checkDiagonal();
+    if (_turnCount === 9) {
+      gameEnd("tie");
+    }
   };
 
   //get current player based on even odd turns
   let setCurrentPlayer = () => {
-    console.log(_turnCount % 2);
     if (_turnCount % 2 === 1) {
       _currentPlayer = "X";
     } else {
@@ -72,10 +100,178 @@ let GameLogic = (() => {
   };
   //starts game loop
   let start = () => {
+    console.log(GameBoard.getGameBoard());
     let playerX = Player("X");
     let playerO = Player("O");
     GameBoard.generateGameBoard();
   };
+  //game over checks
+
+  let checkHorizontal = () => {
+    console.log(GameBoard.getGameBoard());
+    //top row
+    let tempArr = [];
+    for (let i = 0; i < 3; i++) {
+      tempArr.push(
+        GameBoard._gameBoard[i] ? GameBoard._gameBoard[i].class : "l"
+      );
+    }
+    let flatString = tempArr.join("");
+    if (flatString === "XXX") {
+      gameEnd("X");
+    }
+    if (flatString === "OOO") {
+      gameEnd("O");
+    }
+    tempArr = [];
+    /// middle row
+    for (let i = 3; i < 6; i++) {
+      tempArr.push(
+        GameBoard._gameBoard[i] ? GameBoard._gameBoard[i].class : "l"
+      );
+    }
+    flatString = tempArr.join("");
+    if (flatString === "XXX") {
+      gameEnd("X");
+    }
+    if (flatString === "OOO") {
+      gameEnd("O");
+    }
+    tempArr = [];
+    //botttom row
+    for (let i = 6; i < 9; i++) {
+      tempArr.push(
+        GameBoard._gameBoard[i] ? GameBoard._gameBoard[i].class : "l"
+      );
+    }
+    flatString = tempArr.join("");
+    if (flatString === "XXX") {
+      gameEnd("X");
+    }
+    if (flatString === "OOO") {
+      gameEnd("O");
+    }
+    tempArr = [];
+    flatString = "";
+  };
+
+  let checkVertical = () => {
+    //first column
+    let tempArr = [];
+    for (let i = 0; i < GameBoard._gameBoard.length; i += 3) {
+      tempArr.push(
+        GameBoard._gameBoard[i] ? GameBoard._gameBoard[i].class : "l"
+      );
+    }
+    let flatString = tempArr.join("");
+    if (flatString === "XXX") {
+      gameEnd("X");
+    }
+    if (flatString === "OOO") {
+      gameEnd("O");
+    }
+    tempArr = [];
+
+    //second column
+    tempArr = [];
+    for (let i = 1; i < GameBoard._gameBoard.length; i += 3) {
+      tempArr.push(
+        GameBoard._gameBoard[i] ? GameBoard._gameBoard[i].class : "l"
+      );
+    }
+    flatString = tempArr.join("");
+    if (flatString === "XXX") {
+      gameEnd("X");
+    }
+    if (flatString === "OOO") {
+      gameEnd("O");
+    }
+    tempArr = [];
+
+    //third column
+    tempArr = [];
+    for (let i = 2; i < GameBoard._gameBoard.length; i += 3) {
+      tempArr.push(
+        GameBoard._gameBoard[i] ? GameBoard._gameBoard[i].class : "l"
+      );
+    }
+    flatString = tempArr.join("");
+    if (flatString === "XXX") {
+      gameEnd("X");
+    }
+    if (flatString === "OOO") {
+      gameEnd("O");
+    }
+    tempArr = [];
+    flatString = "";
+  };
+
+  let checkDiagonal = () => {
+    // \direction
+    let tempArr = [];
+    for (let i = 0; i < GameBoard._gameBoard.length; i += 4) {
+      tempArr.push(
+        GameBoard._gameBoard[i] ? GameBoard._gameBoard[i].class : "l"
+      );
+    }
+    let flatString = tempArr.join("");
+    if (flatString === "XXX") {
+      gameEnd("X");
+    }
+    if (flatString === "OOO") {
+      gameEnd("O");
+    }
+    tempArr = [];
+    // /direction
+    tempArr = [];
+    for (let i = 2; i < 7; i += 2) {
+      tempArr.push(
+        GameBoard._gameBoard[i] ? GameBoard._gameBoard[i].class : "l"
+      );
+    }
+    flatString = tempArr.join("");
+    if (flatString === "XXX") {
+      gameEnd("X");
+    }
+    if (flatString === "OOO") {
+      gameEnd("O");
+    }
+    tempArr = [];
+    flatString = "";
+  };
+
+  let gameEnd = (result) => {
+    let body = document.querySelector("body");
+    let resetGame = () => {
+      _turnCount = 0;
+      _currentPlayer = "X";
+      GameBoard.resetGameBoard();
+      console.log(GameBoard.getGameBoard());
+      body.removeChild(gameEndContainer);
+      GameBoard.updateDom();
+    };
+    // container
+    let gameEndContainer = document.createElement("div");
+    gameEndContainer.classList.add("game-end-menu");
+    //winner
+    let winner = document.createElement("div");
+    winner.classList.add("winner");
+    if (winner === "tie") {
+      winner.textContent = `It's a ${result}!`;
+    } else {
+      winner.textContent = `${result} is the winner!`;
+    }
+    gameEndContainer.appendChild(winner);
+    //reset button
+    let resetButton = document.createElement("button");
+    resetButton.type = "button";
+    resetButton.textContent = "reset button";
+    resetButton.addEventListener("click", resetGame);
+    gameEndContainer.appendChild(resetButton);
+    //
+    body.appendChild(gameEndContainer);
+  };
+
   return { start, claimTile };
 })();
 
